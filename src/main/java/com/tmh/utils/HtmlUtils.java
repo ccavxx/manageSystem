afdc0ec463a803d4d0102fe1cfd46d86b15ffc38
proteinboy <407122006@qq.com>
2015-12-08 16:41:23 +0800

    m

--- src/main/java/com/tmh/utils/HtmlUtils.java
+++ src/main/java/com/tmh/utils/HtmlUtils.java
@@ -1,12 +1,9 @@
 package com.tmh.utils;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 
 import org.apache.log4j.Logger;
 import org.htmlparser.NodeFilter;
@@ -27,7 +24,7 @@ public class HtmlUtils {
 	
 	
 	/**
-	 * 
+	 * 抓取node节点
 	 * @MethodName:getNodeListByTag
 	 * @param url  抓取的url
 	 * @param tag  html标签名称 table
@@ -70,7 +67,6 @@ public class HtmlUtils {
 	 * @Date 2015年12月2日-下午3:59:07
 	 */
 	public static String getWebCon(String domain){
-//		agentIP();
 		StringBuffer sb = new StringBuffer();
 		Integer responseCode = 0;
 		java.net.URL url = null;
@@ -109,76 +105,6 @@ public class HtmlUtils {
 		return sb.toString();
 	}
 	
-	/**
-     * 向指定 URL 发送POST方法的请求
-     * @param url 发送请求的 URL
-     * @param param 请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
-     * @return 所代表远程资源的响应结果
-     */
-    public static String sendPost(String url, String param) {
-        PrintWriter out = null;
-        BufferedReader in = null;
-        String result = "";
-        try {
-            URL realUrl = new URL(url);
-            // 打开和URL之间的连接
-            URLConnection conn = realUrl.openConnection();
-            // 设置通用的请求属性
-            conn.setRequestProperty("accept", "*/*");
-            conn.setRequestProperty("connection", "Keep-Alive");
-            conn.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            // 发送POST请求必须设置如下两行
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            // 获取URLConnection对象对应的输出流
-            out = new PrintWriter(conn.getOutputStream());
-            // 发送请求参数
-            out.print(param);
-            // flush输出流的缓冲
-            out.flush();
-            // 定义BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream()));
-            String line;
-            while ((line = in.readLine()) != null) {
-                result += line;
-            }
-        } catch (Exception e) {
-            System.out.println("发送 POST 请求出现异常！"+e);
-            e.printStackTrace();
-        }
-        //使用finally块来关闭输出流、输入流
-        finally{
-            try{
-                if(out!=null){
-                    out.close();
-                }
-                if(in!=null){
-                    in.close();
-                }
-            }
-            catch(IOException ex){
-                ex.printStackTrace();
-            }
-        }
-        return result;
-    }
-	
-	/**
-	 * 启动代理ip
-	 * @MethodName:agentIP
-	 * @return void
-	 * @author TianMengHua
-	 * @Date 2015年12月2日-下午4:24:09
-	 */
-	public static void agentIP(){
-		//设置代理
-		System.getProperties().setProperty("proxySet", "true"); //如果不设置，只要代理IP和代理端口正确,此项不设置也可以
-		System.getProperties().setProperty("http.proxyHost", "124.206.133.227");
-		System.getProperties().setProperty("http.proxyPort", "80");
-		logger.info("启用代理IP:" + System.getProperty("http.proxyHost") + " port:" + System.getProperty("http.proxyPort"));
-	}
 	
 	public static void main(String[] args) {
 		System.out.println(getNodeListByTag("http://shbxcx.sn12333.gov.cn/", "h1", "class", "icon001").asString());
