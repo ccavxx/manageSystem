a6ac0e73c01b0983ea9a1bc40572c2327411452e
proteinboy <407122006@qq.com>
2015-12-08 17:06:56 +0800

    m

--- src/main/java/com/tmh/utils/NumberUtils.java
+++ src/main/java/com/tmh/utils/NumberUtils.java
@@ -2,7 +2,6 @@ package com.tmh.utils;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
-import java.util.UUID;
 
 /**
  * 
@@ -31,40 +30,17 @@ public class NumberUtils extends org.apache.commons.lang3.math.NumberUtils{
 	 * @param decimals
 	 * @return
 	 * @return String
-	 * @author kongwei
-	 * @Date 2012-12-20-下午3:33:53
+	 * @author TianMengHua
+	 * @Date 2015年12月8日-下午5:03:44
 	 */
 	public static String decimals2(float decimals) {
 		DecimalFormat dcmFmt = new DecimalFormat("0.00");
 		return dcmFmt.format(decimals);
 	}
 	
-	public static Float decimals2float(float decimals) {
-		DecimalFormat dcmFmt = new DecimalFormat("0.00");
-		return Float.valueOf(dcmFmt.format(decimals));
-	}
-	
-	//DecimalFormat dcmFmt = new DecimalFormat("0.00");
-	
-	public static String randomNum() {
-		return String.valueOf(Math.random() * 1000000).replace(".", "9").substring(0, 6);
-	}
-	 
-	/**
-	 * 上传文件的重命名
-	 * @MethodName:generatefileName
-	 * @return
-	 * @return String
-	 * @author David.Cheng
-	 * @Date 2012-11-21-下午5:27:28
-	 */
-	public static String generatefileName() {
-		return UUID.randomUUID().toString().replace("-","u17");
-	}
-	
 	public static void main(String[] args) {
 		System.err.println(decimals2percent(0.03789f)); ;
-		System.err.println(randomNum());
+		System.out.println(decimals2(0.03789f));
 	}
 	
 	
