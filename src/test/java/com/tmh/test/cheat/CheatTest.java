e733cc5fd74e1abd2fcae9e31e90791d99832158
proteinboy <407122006@qq.com>
2015-12-09 16:35:01 +0800

    m

--- src/test/java/com/tmh/test/cheat/CheatTest.java
+++ src/test/java/com/tmh/test/cheat/CheatTest.java
@@ -32,24 +32,24 @@ public class CheatTest extends AbstractTest{
 	@Test
     public void Chenxiaoge() throws InterruptedException, UnknownHostException{
         
-    	for(int i=0;i<50;i++){
+    	for(int i=0;i<100;i++){
     		
     		//北京市 BGP多线IP
     		AgentIPUtils.agentIP("183.131.151.208", "80"); 
     		
     		//测试当前实际IP
-    		String agentIP = HtmlUtils.getTextByTag("http://city.ip138.com/ip2city.asp", "body");
-    		System.err.println(agentIP);
+//    		String agentIP = HtmlUtils.getTextByTag("http://city.ip138.com/ip2city.asp", "body");
+//    		System.err.println(agentIP);
     		
             //测试显示总投票数
-    		String num = HtmlUtils.getNodeListByTag("http://chengpin.weixmei.com/31cms/31cms/index.php?g=Wap&m=Vote&a=show&token=vjcbgt1414375209&id=65&wecha_id=obRw3t1r2D0I_hBQsI-zseAumHR0&tid=19",
-    	    		   "div", "class", "col-xs-6 counts_btn").elementAt(0).getChildren().asString();
-	        System.out.println("总票数:" + num);
+//    		String num = HtmlUtils.getNodeListByTag("http://chengpin.weixmei.com/31cms/31cms/index.php?g=Wap&m=Vote&a=show&token=vjcbgt1414375209&id=65&wecha_id=obRw3t1r2D0I_hBQsI-zseAumHR0&tid=19",
+//    	    		   "div", "class", "col-xs-6 counts_btn").elementAt(0).getChildren().asString();
+//	        System.out.println("总票数:" + num);
 	    	
 	        //发送 POST 请求
 	        int random = (int)(Math.random()*9000+1000);  //1000~9000之间的随机数
 	        //随机生成微信id
-	        String wecha_id = "3t1r2D0I_hBUI"+ random +"I-zSFAumHR0" ;
+	        String wecha_id = "3t1r2D0I_hBUIA"+ random +"I-SFAumHR0" ;
 	        String s =SendPost.sendPost("http://chengpin.weixmei.com/31cms/index.php?g=Wap&m=Vote&a=toupiao&token=vjcbgt1414375209&id=65&wecha_id=&tid=19",
 	        		"wecha_id=" + wecha_id);
 	        if(s.contains("投票成功,谢谢您的参与")){
@@ -58,7 +58,7 @@ public class CheatTest extends AbstractTest{
 	        	System.out.println("已经投过票了");
 	        }
 	        //随机等待0秒~10秒
-    		Thread.sleep(random);
+//    		Thread.sleep(random);
     		
     		//人工制造异常切换IP
     		AgentIPUtils.agentIP("127.0.0.1", "80"); 
