d362f36743247215c165b4b09eae75ced981360e
tmh <tianmenghua@shanqb.com>
2016-07-08 10:52:02 +0800

    变更

--- src/main/java/com/tmh/web/dao/shard/ShardDao.java
+++ src/main/java/com/tmh/web/dao/shard/ShardDao.java
@@ -1,6 +1,10 @@
 package com.tmh.web.dao.shard;
 
+
+import java.util.List;
+
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 
 import com.tmh.web.dao.base.BaseDao;
 
@@ -8,6 +12,12 @@ public interface ShardDao extends BaseDao{
 	
 	@Insert("INSERT INTO shard_test (name) VALUES (#{name})")
 	public void addShard(String name);
+	
+	@Select("SELECT name FROM shard_test_#{params}")
+	public List<String> getAllShard(Object params);
+	
+	@Select("SELECT count(*) FROM shard_test")
+	public Integer getCountShard();
 	
 	
 }
