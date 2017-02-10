7735abf2c8f2a20e8f8e1252ee8e63139c55d020
tmh <tianmenghua@shanqb.com>
2017-02-10 11:20:37 +0800

    单元测试

---
+++ src/test/java/com/tmh/test/dao/UserDaoTest.java
@@ -0,0 +1,23 @@
+package com.tmh.test.dao;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+
+import com.tmh.test.base.AbstractTest;
+import com.tmh.web.dao.user.UserDao;
+import com.tmh.web.domain.User;
+
+public class UserDaoTest extends AbstractTest{
+	
+	@Autowired
+	private UserDao userDao;
+	
+	@Test
+	@Rollback(false)
+	public void findByUsername(){
+		User user = userDao.findByUsername("admin");
+		System.out.println(user.getId());
+	}
+	
+}
